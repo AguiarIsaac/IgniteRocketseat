38f524fda76e3d923d9666e34b109e420fdaff44
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2022-07-25 14:27:15 +0000

    Layuots home e history

--- Aulas/projeto-02-ignite-timer/src/layouts/DefaultLayout/index.tsx
+++ Aulas/projeto-02-ignite-timer/src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
+import { LayoutContainer } from './styles'
 
 export function DefaoultLayout() {
   return (
-    <>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </>
+    </LayoutContainer>
   )
 }
